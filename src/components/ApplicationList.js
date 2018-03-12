5d4312426725ce6ef35d9d29044682c43e965461
ehdgks0627 <ehdgks0627@naver.com>
2018-03-12 14:01:42 +0900

    Added ApplicationList component

---
+++ src/components/ApplicationList.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import update from 'react-addons-update';
+import axios from 'axios';
+
+import API_SERVER_URL from '../config.js';
+import blankImage from '../static/blank.png';
+import ApplicationPreview from './ApplicationPreview';
+
+class ApplicationList extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+          applications: []
+        };
+    }
+
+    componentDidMount() {
+        axios.get(API_SERVER_URL + '/application')
+            .then((result) => {
+                this.setState({
+                  applications: update(this.state.applications, {$push: result.data})
+                });
+            });
+
+    }
+
+    render() {
+      const mapToComponents = (data) => {
+          data.sort();
+          return data.map((item) => {
+              return (<ApplicationPreview
+                       photo={item.photo ? item.photo : blankImage}
+                       name={item.name}
+                       sid={item.sid} />);
+          });
+      };
+
+        return(
+            <div>{mapToComponents(this.state.applications)}</div>
+        );
+    }
+}
+
+export default ApplicationList;
